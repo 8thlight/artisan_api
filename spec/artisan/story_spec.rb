ea0f468de87d6dffd4005deecb6b76befb537e28
naush <lihsuan@8thlight.com>
2012-06-08 11:57:42 -0500

    Adds acceptance_criteria to stories' attributes

--- spec/artisan/story_spec.rb
+++ spec/artisan/story_spec.rb
@@ -6,12 +6,14 @@ describe Artisan::Story do
     hash = {
       'name'          => 'Test',
       'number'        => 1,
-      'weighted_mean' => nil
+      'weighted_mean' => nil,
+      'acceptance_criteria' => 'Complete this story'
     }
     story = Artisan::Story.new(hash)
     story.name.should == "Test"
     story.optimistic.should be_nil
     story.weighted_mean.should be_nil
+    story.acceptance_criteria.should == "Complete this story"
   end
 
 end
